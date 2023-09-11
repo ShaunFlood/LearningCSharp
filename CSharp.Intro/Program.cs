00fb01cfa39fa26c145e1e18a1c11604c9d02450
ShaunFlood <shaunflood@hotmail.co.uk>
2023-09-11 16:00:56 +0100

    First Task

---
+++ CSharp.Intro/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CSharp.Intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Calculator().Calculate();
+        }
+    }
+
+}
