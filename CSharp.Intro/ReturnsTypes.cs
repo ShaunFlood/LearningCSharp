0bb8ba47af9c215d82bb3a65f003ca636cad6373
ShaunFlood <shaunflood@hotmail.co.uk>
2023-09-11 16:04:42 +0100

    Second task: Understanding Void

---
+++ CSharp.Intro/ReturnsTypes.cs
@@ -0,0 +1,15 @@
+// void is used when the method doesn't need to return anything.
+
+public void Hello() {
+    System.Console.WriteLine("Hello");
+}
+
+// However if the method is returning a specific value it's used instead
+
+public int FavourtieNumber() {
+    return 13;
+}
+
+public string Name() {
+    return "Shaun";
+}
